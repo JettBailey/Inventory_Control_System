601177e093338bf2e4e02d84473778b74d2887ec
Jett Bailey <jbai306@wgu.edu>
2025-02-07 13:27:42 -0700

    Added in an if/else statement to check if inventory has been added already

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -32,93 +32,99 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        long partCount = partRepository.count();
+        long productCount = productRepository.count();
 
-        OutsourcedPart DoorKnob= new OutsourcedPart();
-        DoorKnob.setCompanyName("Hardware Is Us");
-        DoorKnob.setName("Door Knob");
-        DoorKnob.setInv(25);
-        DoorKnob.setPrice(10.0);
-        DoorKnob.setId(1000L);
-        DoorKnob.setMinInv(0);
-        DoorKnob.setMaxInv(50);
-        outsourcedPartRepository.save(DoorKnob);
+        if (partCount == 0 || productCount == 0) {
 
-        OutsourcedPart hinge= new OutsourcedPart();
-        hinge.setCompanyName("Hardware Is Us");
-        hinge.setName("Hinge");
-        hinge.setInv(55);
-        hinge.setPrice(5.0);
-        hinge.setId(1001L);
-        hinge.setMinInv(0);
-        hinge.setMaxInv(50);
-        outsourcedPartRepository.save(hinge);
+            OutsourcedPart DoorKnob = new OutsourcedPart();
+            DoorKnob.setCompanyName("Hardware Is Us");
+            DoorKnob.setName("Door Knob");
+            DoorKnob.setInv(25);
+            DoorKnob.setPrice(10.0);
+            DoorKnob.setId(1000L);
+            DoorKnob.setMinInv(0);
+            DoorKnob.setMaxInv(50);
+            outsourcedPartRepository.save(DoorKnob);
 
-        OutsourcedPart dowel = new OutsourcedPart();
-        dowel.setCompanyName("Wood Suppliers");
-        dowel.setName("Dowel");
-        dowel.setInv(450);
-        dowel.setPrice(0.10);
-        dowel.setId(1002L);
-        dowel.setMinInv(0);
-        dowel.setMaxInv(50);
-        outsourcedPartRepository.save(dowel);
+            OutsourcedPart hinge = new OutsourcedPart();
+            hinge.setCompanyName("Hardware Is Us");
+            hinge.setName("Hinge");
+            hinge.setInv(55);
+            hinge.setPrice(5.0);
+            hinge.setId(1001L);
+            hinge.setMinInv(0);
+            hinge.setMaxInv(50);
+            outsourcedPartRepository.save(hinge);
 
-        OutsourcedPart handle = new OutsourcedPart();
-        handle.setCompanyName("Hardware Is Us");
-        handle.setName("Handle");
-        handle.setInv(20);
-        handle.setPrice(3.0);
-        handle.setId(1003L);
-        handle.setMinInv(0);
-        handle.setMaxInv(50);
-        outsourcedPartRepository.save(handle);
+            OutsourcedPart dowel = new OutsourcedPart();
+            dowel.setCompanyName("Wood Suppliers");
+            dowel.setName("Dowel");
+            dowel.setInv(450);
+            dowel.setPrice(0.10);
+            dowel.setId(1002L);
+            dowel.setMinInv(0);
+            dowel.setMaxInv(50);
+            outsourcedPartRepository.save(dowel);
 
-        OutsourcedPart plank = new OutsourcedPart();
-        plank.setCompanyName("Wood Suppliers");
-        plank.setName("Plank");
-        plank.setInv(74);
-        plank.setPrice(17.0);
-        plank.setId(1004L);
-        plank.setMinInv(0);
-        plank.setMaxInv(50);
-        outsourcedPartRepository.save(plank);
+            OutsourcedPart handle = new OutsourcedPart();
+            handle.setCompanyName("Hardware Is Us");
+            handle.setName("Handle");
+            handle.setInv(20);
+            handle.setPrice(3.0);
+            handle.setId(1003L);
+            handle.setMinInv(0);
+            handle.setMaxInv(50);
+            outsourcedPartRepository.save(handle);
 
-        OutsourcedPart thePart=null;
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            if (part.getName().equals("Door Knob") ||
-                    part.getName().equals("Hinge") ||
-                    part.getName().equals("Dowel") ||
-                    part.getName().equals("Handle") ||
-                    part.getName().equals("Plank")) {
-                thePart = part;
+            OutsourcedPart plank = new OutsourcedPart();
+            plank.setCompanyName("Wood Suppliers");
+            plank.setName("Plank");
+            plank.setInv(74);
+            plank.setPrice(17.0);
+            plank.setId(1004L);
+            plank.setMinInv(0);
+            plank.setMaxInv(50);
+            outsourcedPartRepository.save(plank);
+
+            OutsourcedPart thePart = null;
+            List<OutsourcedPart> outsourcedParts = (List<OutsourcedPart>) outsourcedPartRepository.findAll();
+            for (OutsourcedPart part : outsourcedParts) {
+                if (part.getName().equals("Door Knob") ||
+                        part.getName().equals("Hinge") ||
+                        part.getName().equals("Dowel") ||
+                        part.getName().equals("Handle") ||
+                        part.getName().equals("Plank")) {
+                    thePart = part;
+                }
             }
-        }
 
-        System.out.println(thePart.getCompanyName());
+            System.out.println(thePart.getCompanyName());
 
-        for(OutsourcedPart part:outsourcedParts){
-            System.out.println(part.getName()+" "+part.getCompanyName());
-        }
-
-
-        Product dresser= new Product("dresser",200.0,15);
-        Product desk= new Product("desk",100.0,10);
-        Product table= new Product("table",100.0,10);
-        Product shelf= new Product("shelf",100.0,10);
-        Product door= new Product("door",100.0,10);
-        productRepository.save(dresser);
-        productRepository.save(desk);
-        productRepository.save(table);
-        productRepository.save(shelf);
-        productRepository.save(door);
+            for (OutsourcedPart part : outsourcedParts) {
+                System.out.println(part.getName() + " " + part.getCompanyName());
+            }
 
 
-        System.out.println("Started in Bootstrap");
-        System.out.println("Number of Products"+productRepository.count());
-        System.out.println(productRepository.findAll());
-        System.out.println("Number of Parts"+partRepository.count());
-        System.out.println(partRepository.findAll());
+            Product dresser = new Product("dresser", 200.0, 15);
+            Product desk = new Product("desk", 100.0, 10);
+            Product table = new Product("table", 100.0, 10);
+            Product shelf = new Product("shelf", 100.0, 10);
+            Product door = new Product("door", 100.0, 10);
+            productRepository.save(dresser);
+            productRepository.save(desk);
+            productRepository.save(table);
+            productRepository.save(shelf);
+            productRepository.save(door);
+
+
+            System.out.println("Started in Bootstrap");
+            System.out.println("Number of Products" + productRepository.count());
+            System.out.println(productRepository.findAll());
+            System.out.println("Number of Parts" + partRepository.count());
+            System.out.println(partRepository.findAll());
+
+        } else System.out.println("Inventory already exists");
 
     }
 }
